59f5743b7ead39b89b2f331d7b562e4f1309c93d
tommasoaiello <aiello.tommaso@gmail.com>
2025-06-05 09:24:52 +0200

    add models for new operations

--- server/models.py
+++ server/models.py
@@ -3,3 +3,18 @@ from typing import List
 
 class IssuesList(BaseModel):
     issues: List[str]
+
+class PullRequestList(BaseModel):
+    pull_requests: List[str]
+
+class BranchList(BaseModel):
+    branches: List[str]
+
+class CommitsList(BaseModel):
+    commits: List[str]
+
+class RepoStats(BaseModel):
+    stars: int
+    forks: int
+    watchers: int
+    open_issues: int
